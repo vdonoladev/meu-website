161ef2aa4172f028ccda1315b4fd061025b5d77e
vdonoladev <victordonolaf@gmail.com>
2026-04-04 17:31:20 -0300

    fix: changes to date format and reading time.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,7 +17,7 @@ export function readingTime(html: string) {
   const textOnly = html.replace(/<[^>]+>/g, "")
   const wordCount = textOnly.split(/\s+/).length
   const readingTimeMinutes = ((wordCount / 200) + 1).toFixed()
-  return `${readingTimeMinutes} minutos de leitura`
+  return `${readingTimeMinutes} minuto(s) de leitura`
 }
 
 
