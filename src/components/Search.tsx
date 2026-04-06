fba797c876d96494a8f0df97bcbeb915b0662433
vdonoladev <victordonolaf@gmail.com>
2026-04-05 21:30:14 -0300

    fix: mudança no nome

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -39,7 +39,7 @@ export default function Search({ data }: Props) {
       {(query().length >= 2 && results().length >= 1) && (
         <div class="mt-12">
           <div class="text-sm uppercase mb-2">
-            Encontrados {results().length} resultados para {`'${query()}'`}
+            Encontrados {results().length} resultado(s) para {`'${query()}'`}
           </div>
           <ul class="flex flex-col gap-3">
             {results().map(result => (
